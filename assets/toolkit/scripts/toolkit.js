5b3f33b9ef60951477111d6039322771456f0ff1
Qendrim Sadiku <denisadiki@gmail.com>
2017-04-27 15:59:52 +0200

    Just a Commt

--- assets/toolkit/scripts/toolkit.js
+++ assets/toolkit/scripts/toolkit.js
@@ -13704,7 +13704,7 @@
 /* 318 */
 /***/ (function(module, exports, __webpack_require__) {
 
-	var require;var require;var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;"use strict";
+	var __WEBPACK_AMD_DEFINE_FACTORY__, __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;var require;var require;"use strict";
 
 	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
@@ -16815,7 +16815,9 @@
 	                closeOnConfirm: true
 	              }, function (isConfirmed) {
 	                if (isConfirmed) {
-	                  $(".remove").parent(".pip").remove();
+	                  var rev = $(".remove");
+
+	                  $(rev).parent(".pip").remove();
 	                  swal("Deleted!", "Your imaginary file has been deleted.", "success");
 	                }
 	              });
